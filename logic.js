43935fc1f53bb5463fa087a07797705c8f1f64b5
Manan Garg <manantechy@gmail.com>
2022-04-17 16:34:11 +0530

    some work done in lab

--- logic.js
+++ logic.js
@@ -8,3 +8,32 @@ function toggle(open){
     document.getElementById("missed").style.display="none";
     document.getElementById(open).style.display="block";
 }
+
+function addTask(){
+    alert("Add Task\nSelect date and time <input>");
+
+}
+
+class Task {
+    static no = 0;
+    static coll = [];
+    constructor(desc, date, time) {
+        this.desc = desc;
+        this.due_date = date;
+        this.due_time = time;
+        document.getElementById("all_tasks").innerHTML += `<button type="button" class="collapsible">${this.desc}    ${this.due_date} ${this.due_time}</button><div class="content">${this.desc}</div>`;
+        no++;
+        coll = document.getElementsByClassName("collapsible");
+        coll[no - 1].addEventListener("click", function() {
+            this.classlist.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display=="block"){
+                content.style.display="none";
+            }
+            else{
+                content.style.display="block";
+            }
+        })
+    }
+
+}
