4b521162cea7b204e7dde3f231566e90a591c62a
Manan Garg <manantechy@gmail.com>
2022-04-17 14:11:16 +0530

    adds buttons for tabs and toggle functionality

---
+++ logic.js
@@ -0,0 +1,10 @@
+/*
+functions to toggle b/w tabs
+*/
+
+function toggle(open){
+    document.getElementById("all").style.display="none";
+    document.getElementById("completed").style.display="none";
+    document.getElementById("missed").style.display="none";
+    document.getElementById(open).style.display="block";
+}
