3f280da3d20ee34ae317e373e45c1e4908deb031
Manan Garg <manantechy@gmail.com>
2022-04-18 12:48:41 +0530

    Error: no is not defined on line 54 logic.js even with static no

--- logic.js
+++ logic.js
@@ -8,21 +8,51 @@ function toggle(open){
     document.getElementById("missed").style.display="none";
     document.getElementById(open).style.display="block";
 }
+var alltasks = [];
+function addFinal(){
+    /*
+    do some work
+    */
+   d = new Date(document.getElementById("dt_input").value);
+	//if (Object.prototype.toString.call() === "[object Date]"){
+    if (isNaN(d.getTime()) == false){
+        //do some work and close dialog
+        if (document.getElementById("task_input").value!=""){
+            alltasks.push(new Task(document.getElementById("task_input").value, d));
+            document.getElementById("add").close();
+            document.getElementById("task_input").value="";
+            document.getElementById("dt_input").value="";
+            document.getElementById("dialog_error").innerHTML="";
+            document.getElementById("body").style.display="block";
+            
+            return 0;
+        }
+    }
+    document.getElementById("dialog_error").innerHTML="Invalid Data. Enter again";
+    //}
+    // else{
+    //     console.log('1');
+    // }
+}
 
+function addTaskDialog(){
+    // const d = document.getElementById("body").style.display;
+    // console.log(d);
+    document.getElementById("body").style.display='none';
+    document.getElementById("dialog").style.display="block";
+    document.getElementById("add").show();
 
-function addTask(){
-    document.getElementById("add").show();    
 }
 
 class Task {
-    static no = 0;
+    no = 0;
     static coll = [];
-    constructor(desc, date, time) {
+    constructor(desc, date_time) {
         this.desc = desc;
-        this.due_date = date;
-        this.due_time = time;
+        this.due_date_time = date_time;
         document.getElementById("all_tasks").innerHTML += `<button type="button" class="collapsible">${this.desc}    ${this.due_date} ${this.due_time}</button><div class="content">${this.desc}</div>`;
         no++;
+        console.log(no);
         coll = document.getElementsByClassName("collapsible");
         coll[no - 1].addEventListener("click", function() {
             this.classlist.toggle("active");
@@ -35,5 +65,4 @@ class Task {
             }
         })
     }
-
 }
