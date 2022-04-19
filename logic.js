cfe985fd1c251765f9c1f1e793fd771f470c80a6
Manan Garg <manantechy@gmail.com>
2022-04-19 07:03:40 +0530

    TODO: add functionality to reschedule task, move task from all to complete manually, move to missed automatically

--- logic.js
+++ logic.js
@@ -1,10 +1,11 @@
-/*
-functions to toggle b/w tabs
-*/
-let today = new Date();
 document.getElementById("all").style.display="none";
 document.getElementById("completed").style.display="none";    
 document.getElementById("missed").style.display="none";
+
+var alltasks = [];
+var completedtasks = [];
+var missedtasks = [];
+document.getElementById("all").innerHTML = `You have ${alltasks.length} pending task(s)<br>`+document.getElementById("all").innerHTML;
 
 function moveToComplete(){
 
@@ -16,17 +17,12 @@ function toggle(open){
     document.getElementById("missed").style.display="none";
     document.getElementById(open).style.display="block";
 }
-var alltasks = [];
+
 function addFinal(){
-    /*
-    do some work
-    */
-   d = new Date(document.getElementById("dt_input").value);
-	//if (Object.prototype.toString.call() === "[object Date]"){
+    d = new Date(document.getElementById("dt_input").value);
     if (isNaN(d.getTime()) == false){
-        //do some work and close dialog
-        //compare with current date
-        if (document.getElementById("task_input").value!="" && d<(new Date)){
+        console.log((new Date).toLocaleDateString())
+        if (document.getElementById("task_input").value!="" && d>(new Date)){
             alltasks.push(new Task(document.getElementById("task_input").value, d));
             console.log(alltasks.length);        
             document.getElementById("add").close();
@@ -34,20 +30,13 @@ function addFinal(){
             document.getElementById("dt_input").value="";
             document.getElementById("dialog_error").innerHTML="";
             document.getElementById("body").style.display="block";
-            
             return 0;
         }
     }
     document.getElementById("dialog_error").innerHTML="Invalid Data. Enter again";
-    //}
-    // else{
-    //     console.log('1');
-    // }
 }
 
 function addTaskDialog(){
-    // const d = document.getElementById("body").style.display;
-    // console.log(d);
     document.getElementById("body").style.display='none';
     document.getElementById("dialog").style.display="block";
     document.getElementById("add").show();
@@ -58,7 +47,7 @@ class Task {
     constructor(desc, date_time) {
         this.desc = desc;
         this.due_date_time = date_time;
-        document.getElementById("all_tasks").innerHTML += `<div id="${gf}">${this.desc}    ${this.due_date_time.toUTCString()}<button type="button">Add to Completed</button></div>`;
+        document.getElementById("all_tasks").innerHTML += `<div>${this.desc} <br>due at ${this.due_date_time.toLocaleDateString()} ${this.due_date_time.toLocaleTimeString('en-IN')}<button type="button">Add to Completed</button></div>`;
         console.log(alltasks.length);        
     }
 }
