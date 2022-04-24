ce13d601f66965e28f11a7ad3e5badcbd01470f2
Manan Garg <manantechy@gmail.com>
2022-04-24 16:35:05 +0530

    Error: diff output of item.desc for all items in alltasks array through js file(line 93) and console

--- logic.js
+++ logic.js
@@ -12,17 +12,85 @@ const firebaseConfig = {
 };
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+const ref = db.ref;
 var alltasks = [];
 var completedtasks = [];
 var missedtasks = [];
+var justtasks = [];
 let n = 0;
 document.getElementById("all").innerHTML = `You have ${n} pending task(s)<br>`+document.getElementById("all").innerHTML;
+let taskstring = "";
+getalltasks();
+//document.getElementById("all_tasks").innerHTML=getalltasks();
+// firebase.onValue(ref(db, 'alltasks'), (snapshot) => {
+//     snapshot.forEach((Childsnapshot) =>{ justtasks.push(Childsnapshot.val())});
+//     setDisplay(justtasks);});
 
-let ref = firebase.firestore().collection(db, "alltasks");
 
 
+var docRef = db.collection("alltasks");
+
+// docRef.get().then((doc) => {
+//     if (doc.exists) {
+//         console.log("Document data:", doc.data());
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }
+// }).catch((error) => {
+//     console.log("Error getting document:", error);
+// });
 function moveToComplete(){
 
+};
+
+function getalltasks(){
+    document.getElementById("body").style.display="none";        
+    document.getElementById("loading").style.display="block";
+    let str = "";
+
+    console.log("started");
+    let x = [];
+        console.log("milstone1");
+    db.collection("alltasks").get().then(function(querySnapshot) {
+        x = querySnapshot.docs;
+        console.log(x);
+        document.getElementById("body").style.display="block";        
+        document.getElementById("loading").style.display="none";
+        x.forEach((it) => {docRef = db.collection("alltasks").doc(it.id).get().then((doc)=>{x[x.indexOf(it)]=doc.data()})});
+        console.log("reached");
+        console.log(x);
+        alltasks=x;
+        console.log(alltasks);
+        alltasks.forEach((item)=>{console.log(item.desc);})
+        alltasks.forEach((item)=>{document.getElementById("all").childNodes[5].innerHTML+=(`${item.desc} due at ${item.due_date_time}<br>`);console.log(item.desc);});
+        console.log("done");
+        console.log(str);
+        addToDiv();
+    });
+    
+    // 
+    // db.collection("alltasks").get().then(function(querySnapshot) {      
+    //     x = querySnapshot.docs;
+    //     x.forEach((doc) => {    var docRef = db.collection("cities").doc();
+    // })
+    // });
+    // docRef.get().then((doc) => {
+    //     if (doc.exists) {
+    //         console.log("Document data:", doc.data());
+    //     } else {
+    //         // doc.data() will be undefined in this case
+    //         console.log("No such document!");
+    //     }
+    // }).catch((error) => {
+    //     console.log("Error getting document:", error);
+    // });
+}
+
+function addToDiv(){
+    console.log(alltasks);
+    console.log(alltasks[0].desc);
+    alltasks.forEach((item)=>{console.log(typeof item);console.log(item);console.log(item.desc)})
 }
 
 function toggle(open){
@@ -31,17 +99,17 @@ function toggle(open){
     document.getElementById("missed").style.display="none";
     document.getElementById(open).style.display="block";
 }
-function writeUserData(description, date_time) {
-    db.collection(`alltasks/`).add({
-        desc: description,
-        due_date_time: date_time
-    }).then((docRef)=>{
-        console.log("written with id: ", docRef.id);
-        n++;
-        alltasks.concat([docRef.id]);
-    })
-    .catch((error)=>{console.log("error while writing: ", error);});
-  }
+// function writeUserData(description, date_time) {
+//     db.collection(`alltasks/`).add({
+//         desc: description,
+//         due_date_time: date_time
+//     }).then((docRef)=>{
+//         console.log("written with id: ", docRef.id);
+//         n++;
+//         alltasks.concat([docRef.id]);
+//     })
+//     .catch((error)=>{console.log("error while writing: ", error);});
+//   }
 function addTaskToDB(){
     // d = new Date(document.getElementById("add").childNodes[1].value);
     // if (isNaN(d.getTime())){
@@ -83,7 +151,9 @@ function addTaskToDB(){
                 n++;
                 alltasks = alltasks.concat(`${[docRef.id]}`);
                 console.log(alltasks);
-                document.getElementById("all").innerHTML = `You have ${n} pending task(s)<br>`+document.getElementById("all").innerHTML.slice(33);
+                console.log(document.getElementById("all").innerHTML);
+                document.getElementById("all").innerHTML = `You have ${n} pending task(s)<br>`+document.getElementById("all").innerHTML.slice(33)+`<br><br><div id="${docRef.id}">${alltasks.findIndex(x => x===docRef.id) + 1}.&nbsp;${document.getElementById("task_input").value} due&nbsp;at&nbsp;${d.toUTCString()}</div>`;
+                console.log(document.getElementById("all").innerHTML);
                 document.getElementById("task_input").value="";
                 document.getElementById("dt_input").value="";
                 document.getElementById("dialog_error").innerHTML="";
@@ -129,5 +199,4 @@ class Task {
         });
         completedtasks.push(this);
     }
-    
 }
